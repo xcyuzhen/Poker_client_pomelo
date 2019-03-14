4977d4fe121dfc2b3a2d5add93e9633e8c0ec1b1
xcyuzhen <yuzhenxc@163.com>
2019-03-14 17:49:26 +0800

    更新客户端

--- assets/src/mj/mjOpeMgr.js
+++ assets/src/mj/mjOpeMgr.js
@@ -3,6 +3,15 @@ var BaseMgr = require("./mjBaseMgr");
 cc.Class({
     extends: BaseMgr,
 
+    properties: {
+    	m_container: cc.Node,
+    	m_btnHu: cc.Button,
+    	m_btnPeng: cc.Button,
+    	m_btnGang: cc.Button,
+    	m_btnGuo: cc.Button,
+    	m_gangList: [cc.Node],
+    },
+
     ////////////////////////////////////消息处理函数begin////////////////////////////////////
     ////////////////////////////////////消息处理函数end////////////////////////////////////
 });
