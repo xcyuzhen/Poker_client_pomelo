f97f7b139eeb35c3517cbfd840ba174f46edf1d6
xcyuzhen <yuzhenxc@163.com>
2019-05-30 14:26:39 +0800

    更新客户端

--- assets/src/config/global.js
+++ assets/src/config/global.js
@@ -17,20 +17,17 @@ Global.LoginType = {
 
 Global.GameID = {
 	GT_MJ: 1,
-	GT_DDZ: 2,
 };
 
 //msg分组(和后台配置保持一致)
 Global.MsgGroupName = {
 	HALL: "HallGroup",
 	[Global.GameID.GT_MJ]: "MjGroup",
-	[Global.GameID.GT_DDZ]: "DdzGroup",
 };
 
 //房间场景名字配置
 Global.RoomSceneName = {
 	[Global.GameID.GT_MJ]: "MJRoomScene",
-	[Global.GameID.GT_DDZ]: "DDZRoomScene",
 };
 
 Global.GroupItemResConfig = [
